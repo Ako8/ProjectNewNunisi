a2acb34671c8c8b2592df17aaf7881ef589cb30f
Ako <akogachechiladze22@gmail.com>
2024-06-21 00:07:01 +0400

    made better

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
@@ -9,5 +8,25 @@ def home():
     return render_template('index.html')
 
 
-if __name__  == '__main__':
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/hotel')
+def hotel():
+    return render_template('hotel.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
